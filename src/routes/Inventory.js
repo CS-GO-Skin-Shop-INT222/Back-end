7db36ccef495b7d239734a186ca09502110c7b5e
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2021-10-18 13:39:40 +0700

    [update] item

--- src/routes/Inventory.js
+++ src/routes/Inventory.js
@@ -19,6 +19,23 @@ router.get('/allItem', async (req, res) => {
     
   return res.status(200).send(result)
 })
+router.get('/MyItem/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  const result = await  item.findMany({
+    where:{UserID: id},
+     include: {
+      WeaponSkin:{include:{
+        Skin :{select:{SkinName: true }},
+        Weapon:{select:{WeaponName: true}}
+      }},
+      Users: { select: { Name: true, Email: true } },
+      Item_Sticker: { include: { Sticker: { select: { StickerName: true } } } }
+    }
+  })
+    
+  return res.status(200).send(result)
+})
+
 
 router.get('/getitem/:id', async (req, res) => {
   const id = Number(req.params.id)
--- src/routes/Item.js
+++ src/routes/Item.js
@@ -2,20 +2,31 @@ const router = require("express").Router()
 const { skin } = require('../models/model')
 const { typeOfWeapon } = require('../models/model')
 const { weapon } = require('../models/model')
+const {weaponSkin } = require('../models/model')
 
-router.get('/skin', async (req, res) => {
-    const result = await skin.findMany()
-    return res.status(200).send({msg:'all skin ', Skin :result})
-})
-router.get('/type', async (req, res) => {
-    const result = await typeOfWeapon.findMany()
-    return res.send({msg:'all type ', Type :result})
+router.get('/skin/:id', async (req, res) => {
+    const id = req.params.id
+    const result = await weaponSkin.findMany({
+        where:{WeaponID:id},
+        include:{
+            Skin:{select:{SkinName: true}}
+        },
+        
+    })
+    console.log(result)
+    return res.status(200).send({ Skin :result})
 })
 
-router.get('/weapon', async (req, res) => {
-    const result = await weapon.findMany()
-    return res.send({msg:'all weapon ', Weapon :result})
+router.get('/weapon/:id', async (req, res) => {
+    const id = req.params.id
+    const result = await weapon.findMany({
+        where:{TypeID :id}
+    })
+    return res.send({ Weapon :result})
 })
+
+
+
 
 
 module.exports = router;
