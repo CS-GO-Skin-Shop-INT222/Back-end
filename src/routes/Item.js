2a90efcf45ec74b74105042009d1d981aa6d626d
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2021-11-04 13:26:59 +0700

    [add] get allweaponskin

--- src/routes/Item.js
+++ src/routes/Item.js
@@ -4,16 +4,27 @@ const { typeOfWeapon } = require('../models/model')
 const { weapon } = require('../models/model')
 const { weaponSkin } = require('../models/model')
 
-router.get('/skin/:id', async (req, res) => {
+router.get('/showweapon/:id', async (req, res) => {
     const id = req.params.id
     const result = await weaponSkin.findMany({
         where: { WeaponID: id },
         include: {
             Skin: { select: { SkinName: true } }
-        },
+        }
 
     })
-    return res.status(200).send({ Skin: result })
+    return res.status(200).send({ Weapon : result })
+})
+
+router.get('/showskin/:id', async (req, res) =>{
+    const id = req.params.id
+    const result = await weaponSkin.findMany({
+        where: { SkinID: id },
+        include: {
+            Weapon: { select: { WeaponName: true } }
+        }
+    })
+    return res.status(200).send({ Weapon : result })
 })
 
 router.get('/weapon/:id', async (req, res) => {
@@ -23,6 +34,13 @@ router.get('/weapon/:id', async (req, res) => {
     })
     return res.send({ Weapon: result })
 })
+
+router.get('/allweaponskin', async (req, res) => {
+    const result = await weaponSkin.findMany()
+    return res.status(200).send({ msg: 'all skin ', Skin: result })
+})
+
+
 
 router.get('/allskin', async (req, res) => {
     const result = await skin.findMany()
