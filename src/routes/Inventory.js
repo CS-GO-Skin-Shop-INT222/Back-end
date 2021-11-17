8d0dc9b7290a45de8067d98ae46fb82516bf9d04
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2021-11-17 14:20:15 +0700

    [add] api inventory

--- src/routes/Inventory.js
+++ src/routes/Inventory.js
@@ -5,7 +5,7 @@ const dayjs = require("dayjs")
 let lastUpdate = dayjs(Date.now()).format()
 const { verifyTokenUser } = require('../middleware/auth')
 
-router.get('/allItem', async (req, res) => {
+router.get('/allItem',verifyTokenUser, async (req, res) => {
 
   const result = await item.findMany({
     include: {
@@ -36,7 +36,7 @@ router.get('/MyItem/:id/:page',verifyTokenUser, async (req, res) => {
     const result = await item.findMany({
       skip: calSkip(page, numberOfItem),
       take: numberOfItem,
-      where: { UserID: id },
+      where: { UserID: id , Publish : false},
       include: {
         WeaponSkin: {
           include: {
@@ -55,8 +55,40 @@ router.get('/MyItem/:id/:page',verifyTokenUser, async (req, res) => {
   }
 })
 
+router.get('/MyItemselling/:id/:page',verifyTokenUser, async (req, res) => {
+  try {
+    const calSkip = (page, numberOfItem) => {
+      return (page - 1) * numberOfItem
+    }
+    const CalPage = (item, numberOfItem) => {
+      return Math.ceil(item / numberOfItem)
+    }
+    let page = Number(req.params.page)
+    let numberOfItem = 9
+    const id = Number(req.params.id)
+    const result = await item.findMany({
+      skip: calSkip(page, numberOfItem),
+      take: numberOfItem,
+      where: { UserID: id , Publish : true},
+      include: {
+        WeaponSkin: {
+          include: {
+            Skin: { select: { SkinName: true } },
+            Weapon: { select: { WeaponName: true } }
+          }
+        },
+        Users: { select: { Name: true, Email: true } },
+        Item_Sticker: { include: { Sticker: { select: { StickerName: true } } } }
+      }
+    })
+    const totalItem = await item.count()
+    return res.status(200).send({ data: result, page: page, totalpage: CalPage(totalItem, numberOfItem) })
+  } catch (error) {
+    res.status(500).end("error")
+  }
+})
 
-router.get('/getitem/:id', async (req, res) => {
+router.get('/getitem/:id',verifyTokenUser, async (req, res) => {
   const id = Number(req.params.id)
   const result = await item.findUnique({
     where: { ItemID: id },
@@ -74,7 +106,7 @@ router.get('/getitem/:id', async (req, res) => {
   return res.status(200).send(result)
 })
 
-router.post('/addItem', async (req, res) => {
+router.post('/addItem',verifyTokenUser, async (req, res) => {
   let { Price, Description, UserID, WeaponSkinID, Stickers } = req.body
   if (!(Price && Description && WeaponSkinID && UserID)) {
     return res.status(500).send({ msg: "Please input item information!" })
@@ -100,7 +132,7 @@ router.post('/addItem', async (req, res) => {
   return res.status(200).send({ msg: "Create successfully" })
 })
 
-router.put('/editItem/:id', async (req, res) => {
+router.put('/editItem/:id',verifyTokenUser, async (req, res) => {
   let id = Number(req.params.id)
   const result = await item.updateMany({
     data: req.body,
@@ -112,7 +144,7 @@ router.put('/editItem/:id', async (req, res) => {
   return res.status(200).send(result)
 })
 
-router.put('/sellItem/:id', async (req, res) => {
+router.put('/sellItem/:id',verifyTokenUser, async (req, res) => {
   let id = Number(req.params.id)
   const result = await item.update({
     where: { ItemID: id },
@@ -125,11 +157,26 @@ router.put('/sellItem/:id', async (req, res) => {
     return res.status(500).send({ msg: "the item is sell" })
   
   }
-  return res.send({ msg: "this item is selling!" })
+  return res.status(200).send({ msg: "this item is selling!" })
+})
+router.put('/cancelsales/:id',verifyTokenUser, async (req, res) => {
+  let id = Number(req.params.id)
+  const result = await item.updateMany({
+    data: req.body,
+    where: { ItemID: id },
+    data: { Publish: false }
+  })
+  if (result.count == 0) {
+    return res.status(500).send({ msg: "Don't have item" })
+  }
+  if(result.Publish=true){
+    return res.status(500).send({ msg: "this item is not sell" })
+  
+  }
+  return res.status(200).send({ msg: "cancel finish!" })
 })
 
-
-router.delete("/deleteItem/:id", async (req, res) => {
+router.delete("/deleteItem/:id",verifyTokenUser, async (req, res) => {
   const id = Number(req.params.id)
   const result = await item.deleteMany({
     where: { ItemID: id }
